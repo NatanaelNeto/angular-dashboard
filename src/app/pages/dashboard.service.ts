7d34092f66297d42e23a6b2aeeddd972d54534ae
Natanael Neto <natanael.eneas2018@hotmail.com>
2023-07-28 16:03:51 -0300

    C0 - Impression e Coin

--- src/app/pages/dashboard.service.ts
+++ src/app/pages/dashboard.service.ts
@@ -9,11 +9,22 @@ import Cryptocoin from '../utils/Cryptocoin';
 
 export class DashboardService {
 
-  private readonly DATA = '/assets/data/cryptocoins.json';
+  private readonly CRYPTOCOIN_DATA = '/assets/data/cryptocoins.json';
+  private readonly IMPRESSION_DATA = '/assets/data/impression.json';
+  private readonly PORTFOLIO_DATA = '/assets/data/portfolio.json';
+
 
   constructor(private http: HttpClient) { }
 
   getCryptos(): Observable<{ coins: Cryptocoin[] }> {
-    return this.http.get<{ coins: Cryptocoin[] }>(this.DATA);
+    return this.http.get<{ coins: Cryptocoin[] }>(this.CRYPTOCOIN_DATA);
+  }
+
+  getImpression(): Observable<{ weekly: number[], monthly: number[] }> {
+    return this.http.get<{ weekly: number[], monthly: number[] }>(this.IMPRESSION_DATA);
+  }
+
+  getPortfolio(): Observable<{ portfolio: { coin: string, value: number, usd: number }[] }> {
+    return this.http.get<{ portfolio: { coin: string, value: number, usd: number }[] }>(this.PORTFOLIO_DATA);
   }
 }
