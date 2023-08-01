f0c2f62fa39a29e14075b7be7190aeba5b23970a
Natanael Neto <natanael.eneas2018@hotmail.com>
2023-08-01 12:06:29 -0300

    C3 - Responsividade para celulares

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -26,10 +26,19 @@ export class SidebarComponent implements OnInit {
   }
 
   sidebarActive(): Boolean {
-    return this.service.getActive();
+    return window.innerWidth > 800 ? this.service.getActive() : !this.service.getActive();
   }
 
   darkmode(): Boolean {
     return this.service.isDarkMode();
+  }
+
+  mobileVH(): Object {
+    // First we get the viewport height and we multiple it by 1% to get a value for a vh unit
+    let vh = window.innerHeight * 0.01;
+    return {
+      '--vh': `${vh}px`,
+    };
+    // Then we set the value in the --vh custom property to the root of the document
   }
 }
